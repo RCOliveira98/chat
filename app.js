9093324dfd39e748e53e6c6c6ccf038e6929e0cb
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-18 11:26:06 -0300

    Conectando usuários do chat.

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ var server = app.listen(3000, function() {
     console.log('Servidor online');
 });
 
-require('socket.io').listen(server);
+io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+// criar conection por websocket
+// estamos ouvindo eventos de conexão
+io.on('connection', function(my_socket) {
+    console.log('user conectou');
+
+    my_socket.on('disconnect', () => console.log('user desconectado'));
+});
+
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,9 @@
 module.exports.get_chat = function(aplication, req, resp) {
+    var user = req.body;
+    // io é uma var global criada no app.js
+    aplication.get('io')
+    .emit('msgToClient',
+    user.apelido);
+    
     resp.render('chat');
 }
